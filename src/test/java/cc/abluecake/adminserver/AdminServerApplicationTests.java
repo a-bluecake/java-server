80d089ac21a06f21184e1738821c9556a42f3da1
Furobot <2585557937@qq.com>
2026-08-22 16:58:54 +0800

    Seventh-连接redis缓存数据库，完成redis序列化

--- src/test/java/cc/abluecake/adminserver/AdminServerApplicationTests.java
+++ src/test/java/cc/abluecake/adminserver/AdminServerApplicationTests.java
@@ -2,6 +2,7 @@ package cc.abluecake.adminserver;
 
 import cc.abluecake.adminserver.entity.User;
 import cc.abluecake.adminserver.mapper.UserMapper;
+import cc.abluecake.adminserver.util.RedisUtil;
 import com.baomidou.mybatisplus.core.toolkit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @SpringBootTest
 class AdminServerApplicationTests {
+
     @Autowired
     private UserMapper userMapper;
 
@@ -22,4 +24,16 @@ class AdminServerApplicationTests {
         userList.forEach(System.out::println);
     }
 
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @Test
+    public void testRedis() {
+        System.out.println(("----- redis method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        // set方法存数据，键为userList
+        redisUtil.set("userList", userList);
+        // get方法从键userList中取数据
+        System.out.println(redisUtil.get("userList"));
+    }
 }
